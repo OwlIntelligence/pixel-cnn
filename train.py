6d865f6731bab5d7d85160be86e5ff8dc486843a
Jin Xu <s1673820@sms.ed.ac.uk>
2018-02-21 22:54:45 +0800

    sync

--- train.py
+++ train.py
@@ -312,7 +312,7 @@ with tf.Session() as sess:
             # generate samples from the model
             sample_x = []
             for i in range(args.num_samples):
-                sample_x.append(sample_from_model(sess, data=next(train_data))) ##
+                sample_x.append(sample_from_model(sess, data=next(test_data))) ##
             sample_x = np.concatenate(sample_x,axis=0)
             img_tile = plotting.img_tile(sample_x[:100], aspect_ratio=1.0, border_color=1.0, stretch=True)
             img = plotting.plot_img(img_tile, title=args.data_set + ' samples')

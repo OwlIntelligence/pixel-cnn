4a08da4261393740866494f43947b795872da90a
Jin Xu <s1673820@sms.ed.ac.uk>
2018-02-21 17:06:50 +0800

    sync

--- utils/mfunc.py
+++ utils/mfunc.py
@@ -4,9 +4,7 @@ from utils.mask import *
 def mask_inputs(inputs, mgen):
     batch_size, height, width, num_channel = inputs.shape
     masks = mgen.gen(batch_size)
-    print(masks.shape)
-    print(np.expand_dims(masks, axis=-1).shape)
-    inputs = np.concatenate([inputs, np.expand_dims(masks, axis=0)], axis=-1)
+    inputs = np.concatenate([inputs, np.expand_dims(masks, axis=-1)], axis=-1)
     for c in range(num_channel):
         inputs[:, :, :, c] *= masks
     return inputs

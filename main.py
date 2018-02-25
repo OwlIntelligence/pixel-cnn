4d2b255358986e2757a16846b42a2708eca2e9e4
Jin Xu <s1673820@sms.ed.ac.uk>
2018-02-25 11:39:50 +0800

    sync

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ args = parser.parse_args()
 #config_args(args, configs['cifar'])
 config_args(args, configs[args.config_name])
 print('input args:\n', json.dumps(vars(args), indent=4, separators=(',',':'))) # pretty print args
-exp_label = "celeba64-center"
+exp_label = "celeba64-eye"
 
 # -----------------------------------------------------------------------------
 # fix random seed for reproducibility
@@ -191,8 +191,8 @@ bits_per_dim_test = loss_gen_test[0]/(args.nr_gpu*np.log(2.)*np.prod(obs_shape)*
 
 # mask generator
 train_mgen = um.RandomRectangleMaskGenerator(obs_shape[0], obs_shape[1])
-test_mgen = um.CenterMaskGenerator(obs_shape[0], obs_shape[1])
-#test_mgen = um.RectangleMaskGenerator(obs_shape[0], obs_shape[1], (28, 62, 30, 2))
+#test_mgen = um.CenterMaskGenerator(obs_shape[0], obs_shape[1])
+test_mgen = um.RectangleMaskGenerator(obs_shape[0], obs_shape[1], (28, 62, 30, 2))
 
 # sample from the model
 def sample_from_model(sess, data=None):

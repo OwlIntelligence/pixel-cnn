96a2214f206564acf37252eb8bb0efd07c8f36e7
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-07 20:01:25 +0800

    sync

--- train.py
+++ train.py
@@ -63,6 +63,7 @@ parser.add_argument('-gd', '--global_latent_dim', type=int, default=10, help='di
 parser.add_argument('-sn', '--spatial_latent_num_channel', type=int, default=4, help='number of channels for spatial latent variables')
 parser.add_argument('-is', '--input_size', type=int, default=-1, help='input size')
 parser.add_argument('-cc', '--context_conditioning', dest='context_conditioning', action='store_true', help='Condition on context (masked inputs)?')
+parser.add_argument('-dg', '--debug', dest='debug', action='store_true', help='Under debug mode?')
 #
 args = parser.parse_args()
 
@@ -104,10 +105,16 @@ elif 'celeba' in args.data_set:
 else:
     raise("unsupported dataset")
 if args.data_set=='celeba128':
-     train_data = DataLoader(args.data_dir, 'test', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional, size=128)
-     test_data = DataLoader(args.data_dir, 'test', args.batch_size * args.nr_gpu, shuffle=False, return_labels=args.class_conditional, size=128)
+    if args.debug:
+        train_data = DataLoader(args.data_dir, 'valid', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional, size=128)
+    else:
+        train_data = DataLoader(args.data_dir, 'train', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional, size=128)
+    test_data = DataLoader(args.data_dir, 'test', args.batch_size * args.nr_gpu, shuffle=False, return_labels=args.class_conditional, size=128)
 else:
-    train_data = DataLoader(args.data_dir, 'train', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional)
+    if args.debug:
+        train_data = DataLoader(args.data_dir, 'valid', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional)
+    else:
+        train_data = DataLoader(args.data_dir, 'train', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional)
     test_data = DataLoader(args.data_dir, 'test', args.batch_size * args.nr_gpu, shuffle=False, return_labels=args.class_conditional)
 img_shape = train_data.get_observation_size() # e.g. a tuple (32,32,3)
 if args.input_size < 0:
@@ -228,14 +235,14 @@ def sample_from_model(sess, data=None):
     if data is not None and type(data) is not tuple:
         x = data
     x = np.cast[np.float32]((x - 127.5) / 127.5)
-    g = grid.generate_grid((x.shape[1], x.shape[2]))
-    x = np.concatenate([x, np.broadcast_to(g, shape=(x.shape[0],g.shape[0],g.shape[1],g.shape[2]))], axis=-1)
+    g = grid.generate_grid((x.shape[1], x.shape[2]), batch_size=x.shape[0])
+    xg = np.concatenate([x, g], axis=-1)
+    xg, _ = uf.random_crop_images(xg, output_size=(args.input_size, args.input_size))
+    x, g = xg[:, :, :, :3], xg[:, :, :, 3:]
     print(x.shape)
-    quit()
-
-    #x, y = uf.random_crop_images(x, output_size=(args.input_size, args.input_size))
+    print(g.shape)
     x = np.split(x, args.nr_gpu)
-    y = np.split(y, args.nr_gpu)
+    # y = np.split(y, args.nr_gpu)
 
     h = [x[i].copy() for i in range(args.nr_gpu)]
     for i in range(args.nr_gpu):
@@ -296,11 +303,13 @@ def make_feed_dict(data, init=False):
         y = None
 
     x = np.cast[np.float32]((x - 127.5) / 127.5) # input to pixelCNN is scaled from uint8 [0,255] to float in range [-1,1]
-    g = grid.generate_grid((x.shape[1], x.shape[2]))
-    x = np.concatenate([x, np.broadcast_to(g, shape=(x.shape[0],g.shape[0],g.shape[1],g.shape[2]))], axis=-1)
+    g = grid.generate_grid((x.shape[1], x.shape[2]), batch_size=x.shape[0])
+    xg = np.concatenate([x, g], axis=-1)
+    xg, _ = uf.random_crop_images(xg, output_size=(args.input_size, args.input_size))
+    x, g = xg[:, :, :, :3], xg[:, :, :, 3:]
     print(x.shape)
-    quit()
-    #x, y = uf.random_crop_images(x, output_size=(args.input_size, args.input_size))
+    print(g.shape)
+
 
     if init:
         feed_dict = {x_init: x}
--- utils/grid.py
+++ utils/grid.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 
-def generate_grid(img_size, value_interval=[-1,1]):
+def generate_grid(img_size, value_interval=[-1,1], batch_size=None):
     h = np.linspace(start=value_interval[0], stop=value_interval[1], num=img_size[0], dtype=np.float32)
     w = np.linspace(start=value_interval[0], stop=value_interval[1], num=img_size[1], dtype=np.float32)
     grid = np.zeros((img_size[0], img_size[1], 2), dtype=np.float32)
     for i in range(img_size[0]):
         for j in range(img_size[1]):
             grid[i, j, :] = [h[i], w[j]]
-    return grid
+    if batch_size is None:
+        return grid
+    else:
+        return np.broadcast_to(grid, shape=(batch_size,)+grid.shape)

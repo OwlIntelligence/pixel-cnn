7b0c4381bb2d163c482b40fb18ec618d403e3adb
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-10 16:20:20 +0800

    sync

--- vae.py
+++ vae.py
@@ -74,6 +74,8 @@ def discretized_mix_logistic_loss(x,l,sum_all=True):
     log_probs = tf.where(x < -0.999, log_cdf_plus, tf.where(x > 0.999, log_one_minus_cdf_min, tf.where(cdf_delta > 1e-5, tf.log(tf.maximum(cdf_delta, 1e-12)), log_pdf_mid - np.log(127.5))))
 
     log_probs = tf.reduce_sum(log_probs,3) + log_prob_from_logits(logit_probs)
+    print(log_probs)
+    quit()
     if sum_all:
         return -tf.reduce_sum(log_sum_exp(log_probs))
     else:

d906f5692758b12a2382515878ceb1a06bf56a57
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-10 20:29:36 +0800

    sync

--- pixel_cnn_pp/nn.py
+++ pixel_cnn_pp/nn.py
@@ -80,9 +80,10 @@ def discretized_mix_logistic_loss(x,l,sum_all=True, masks=None):
     # the 1e-12 in tf.maximum(cdf_delta, 1e-12) is never actually used as output, it's purely there to get around the tf.select() gradient issue
     # if the probability on a sub-pixel is below 1e-5, we use an approximation based on the assumption that the log-density is constant in the bin of the observed sub-pixel value
     log_probs = tf.where(x < -0.999, log_cdf_plus, tf.where(x > 0.999, log_one_minus_cdf_min, tf.where(cdf_delta > 1e-5, tf.log(tf.maximum(cdf_delta, 1e-12)), log_pdf_mid - np.log(127.5))))
-
-    log_probs = tf.reduce_sum(log_probs,3) + log_prob_from_logits(logit_probs)
+    print(log_probs)
     return log_probs
+    log_probs = tf.reduce_sum(log_probs,3) + log_prob_from_logits(logit_probs)
+
     lse = log_sum_exp(log_probs)
     if masks is not None:
         assert lse.shape==masks.shape, "shape of masks does not match the log_sum_exp outputs"

79d19f00fbdc7ca937c3c3108ad7d720a29bdd38
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-11 13:06:01 +0800

    sync

--- vae.py
+++ vae.py
@@ -142,7 +142,6 @@ with tf.Session(config=config) as sess:
             data = np.cast[np.float32]((data - 127.5) / 127.5)
             feed_dict = {x: data}
             l, _ = sess.run([loss, train_step], feed_dict=feed_dict)
-            print(l)
             train_loss_epoch.append(l)
         train_loss_epoch = np.mean(train_loss_epoch)
 

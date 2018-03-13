c60bba56e208484ec9487cd6739a6afb41fbbde4
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-13 20:59:34 +0800

    sync

--- vae.py
+++ vae.py
@@ -14,6 +14,7 @@ tf.flags.DEFINE_integer("batch_size", default_value=100, docstring="")
 tf.flags.DEFINE_integer("nr_gpu", default_value=2, docstring="number of GPUs")
 tf.flags.DEFINE_string("data_dir", default_value="/data/ziz/not-backed-up/jxu/CelebA", docstring="")
 tf.flags.DEFINE_string("save_dir", default_value="/data/ziz/jxu/models/vae-test", docstring="")
+tf.flags.DEFINE_string("data_set", default_value="celeba128", docstring="")
 tf.flags.DEFINE_boolean("load_params", default_value=False, docstring="load_parameters from save_dir?")
 
 FLAGS = tf.flags.FLAGS

c8e77757c2fe1cad8917220d52a2d71e68ef3934
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-15 10:11:14 +0800

    sync

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import data.celeba_data as celeba_data
 import vae_loading as v
 import utils.mask as m
 from utils import plotting
-
+import utils.mfunc as uf
 
 
 test_data = celeba_data.DataLoader(v.FLAGS.data_dir, 'valid', v.FLAGS.batch_size*v.FLAGS.nr_gpu, shuffle=False, size=128)
@@ -20,6 +20,13 @@ with tf.Session(config=config) as sess:
     test_mgen = m.CenterMaskGenerator(128, 128, 0.75)
 
     data = next(test_data)
+    data = uf.mask_inputs(data, test_mgen)
+
+    img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
+    img = plotting.plot_img(data, title=v.FLAGS.data_set + ' samples')
+    plotting.plt.savefig(os.path.join("plots",'%s_vae_original.png' % (v.FLAGS.data_set)))
+    
+
     feed_dict = v.make_feed_dict(data, test_mgen)
     sample_x = sess.run(v.x_hats, feed_dict=feed_dict)
     sample_x = np.concatenate(sample_x, axis=0)

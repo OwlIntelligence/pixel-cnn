ff5c1040dbc0c84c95e4dcac79067dbb7e751483
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-15 11:25:03 +0800

    sync

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ with tf.Session(config=config) as sess:
 
     v.load_vae(sess, v.saver)
 
-    test_mgen = m.CenterMaskGenerator(128, 128, 1.)
+    test_mgen = m.CenterMaskGenerator(128, 128, 120. / 128.)
 
     data = next(test_data)
     data = uf.mask_inputs(data, test_mgen)[:,:,:,:3]

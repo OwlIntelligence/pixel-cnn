80e98a90fd2f5da009a2facb6153851cd47adfab
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-15 10:15:17 +0800

    sync

--- test.py
+++ test.py
@@ -23,9 +23,9 @@ with tf.Session(config=config) as sess:
     data = uf.mask_inputs(data, test_mgen)
 
     img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
-    img = plotting.plot_img(data, title=v.FLAGS.data_set + ' samples')
+    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
     plotting.plt.savefig(os.path.join("plots",'%s_vae_original.png' % (v.FLAGS.data_set)))
-    
+
 
     feed_dict = v.make_feed_dict(data, test_mgen)
     sample_x = sess.run(v.x_hats, feed_dict=feed_dict)

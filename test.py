f6d4c6b3f80f025c540d9d0c89f7cbbac27f0f53
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-15 10:56:31 +0800

    sync

--- test.py
+++ test.py
@@ -20,16 +20,19 @@ with tf.Session(config=config) as sess:
     test_mgen = m.CenterMaskGenerator(128, 128, 0.75)
 
     data = next(test_data)
-    data = uf.mask_inputs(data, test_mgen)[:,:,:,:3]
+    # data = uf.mask_inputs(data, test_mgen)[:,:,:,:3]
 
-    img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
-    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
-    plotting.plt.savefig(os.path.join("plots",'%s_vae_original.png' % (v.FLAGS.data_set)))
+    # img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
+    # img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
+    # plotting.plt.savefig(os.path.join("plots",'%s_vae_original.png' % (v.FLAGS.data_set)))
 
 
     feed_dict = v.make_feed_dict(data, test_mgen)
-    sample_x = sess.run(v.x_hats, feed_dict=feed_dict)
+    #sample_x = sess.run(v.x_hats, feed_dict=feed_dict)
+    sample_x = sess.run(v.z, feed_dict=feed_dict)
     sample_x = np.concatenate(sample_x, axis=0)
+    print(sample_x)
+    quit()
     test_data.reset()
 
     img_tile = plotting.img_tile(sample_x[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)

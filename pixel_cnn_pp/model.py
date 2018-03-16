153ddfaffb5d72ffa1bd93f7e4294f1a4e20d9bf
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-16 20:26:19 +0800

    sync

--- pixel_cnn_pp/model.py
+++ pixel_cnn_pp/model.py
@@ -31,11 +31,11 @@ def model_spec(x, gh=None, sh=None, init=False, ema=None, dropout_p=0.5, nr_resn
 
         if spatial_conditional:
             if type(sh)==list:
+                sh, sh_2, sh_3 = sh
+            else:
                 sh = nn.latent_deconv_net(sh, scale_factor=1)
                 sh_2 = nn.conv2d(sh, nn.int_shape(sh)[-1], filter_size=[3,3], stride=[2,2], pad='SAME')
                 sh_4 = nn.conv2d(sh_2, nn.int_shape(sh)[-1], filter_size=[3,3], stride=[2,2], pad='SAME')
-            else:
-                sh, sh_2, sh_3 = sh
         else:
             sh_2, sh_4 = None, None
 

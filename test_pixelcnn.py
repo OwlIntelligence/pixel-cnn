785654134e817d2dc8dcfc3dc1b68a45bb85cd3a
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-16 11:42:03 +0800

    sync

--- test_pixelcnn.py
+++ test_pixelcnn.py
@@ -246,10 +246,9 @@ def sample_from_model(sess, data=None, **params):
 
 
     x = np.split(x, args.nr_gpu)
-    #x_gen = [np.zeros_like(x[0]) for i in range(args.nr_gpu)]
-    x_gen = x #[np.zeros_like(x[0]) for i in range(args.nr_gpu)]
+    x_gen = [np.zeros_like(x[0]) for i in range(args.nr_gpu)]
 
-    for yi in range(16, obs_shape[0]):
+    for yi in range(obs_shape[0]):
         for xi in range(obs_shape[1]):
             feed_dict.update({xs[i]: x_gen[i] for i in range(args.nr_gpu)})
             new_x_gen_np = sess.run(new_x_gen, feed_dict=feed_dict)

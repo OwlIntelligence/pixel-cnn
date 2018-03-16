68372b6ef91d8dacc64cf962bddcd445e3bbee82
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-16 22:18:56 +0800

    sync

--- train.py
+++ train.py
@@ -17,7 +17,8 @@ import numpy as np
 import tensorflow as tf
 
 from pixel_cnn_pp import nn
-from pixel_cnn_pp.model import model_spec
+# from pixel_cnn_pp.model import model_spec
+from pixel_cnn_pp.mini_model import model_spec
 from utils import plotting
 import utils.mask as um
 import utils.mfunc as uf

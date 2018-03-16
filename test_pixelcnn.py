fe122031ab276104470b0d113332636f0841b2e9
Jin Xu <s1673820@sms.ed.ac.uk>
2018-03-16 17:11:22 +0800

    sync

--- test_pixelcnn.py
+++ test_pixelcnn.py
@@ -391,7 +391,7 @@ with tf.Session(config=config) as sess:
     saver.restore(sess, ckpt_file)
 
     d = next(test_data)
-    sample_mgen = um.CenterMaskGenerator(128, 128, 8./128)
+    sample_mgen = um.CenterMaskGenerator(128, 128, 48./128)
     mask = sample_mgen.gen(1)[0]
 
     feed_dict = vl.make_feed_dict(d)

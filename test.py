c1da1fafbc42386e4601f946be690d88452db269
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-17 16:22:56 +0800

    sync

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ with tf.Session(config=config) as sess:
     feed_dict = v.make_feed_dict(data, test_mgen)
     sample_x = sess.run(v.x_hats, feed_dict=feed_dict)
     sample_x = np.concatenate(sample_x, axis=0)
-    sample_mask = test_mgen.gen(1)[0]
+    sample_mask = uf.broadcast_mask(test_mgen.gen(1)[0], 3, data.shape[0])
     data = np.cast[np.float32](data/255.)
     sample_x = sample_x * (1-sample_mask) + data * sample_mask
     test_data.reset()

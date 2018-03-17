507f0a758ff88f2f801f4551aaed272dccde76de
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-17 17:02:38 +0800

    sync

--- configs.py
+++ configs.py
@@ -95,7 +95,7 @@ configs['celeba128'] = {
 
 configs['celeba128-patch'] = {
     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/models/celeba128-patch-1",
+    "save_dir": "/data/ziz/jxu/models/celeba128-patch-2",
     "nr_filters": 100,
     "nr_resnet": 4,
     "data_set": "celeba128",

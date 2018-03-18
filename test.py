066e6b617991975cd845800236332b318fd8dc44
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-18 18:51:37 +0800

    sync

--- test.py
+++ test.py
@@ -34,9 +34,17 @@ with tf.Session(config=config) as sess:
     sample_x = sess.run(v.x_hats, feed_dict=feed_dict)
     sample_x = np.concatenate(sample_x, axis=0)
 
+    img_tile = plotting.img_tile(sample_x[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
+    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
+    plotting.plt.savefig(os.path.join("plots",'%s_vae_recon.png' % (v.FLAGS.data_set)))
+
     data = np.cast[np.float32](data/255.)
     sample_x = sample_x - data
 
     img_tile = plotting.img_tile(sample_x[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
     img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
     plotting.plt.savefig(os.path.join("plots",'%s_vae_diff.png' % (v.FLAGS.data_set)))
+
+    img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
+    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
+    plotting.plt.savefig(os.path.join("plots",'%s_vae_ori.png' % (v.FLAGS.data_set)))

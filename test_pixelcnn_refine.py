f203f7447450721d72a3fab7d31ebc5e8f14c3ed
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-18 11:36:41 +0800

    sync

--- test_pixelcnn_refine.py
+++ test_pixelcnn_refine.py
@@ -414,7 +414,7 @@ with tf.Session(config=config) as sess:
     saver.restore(sess, ckpt_file)
 
     d = next(test_data)
-    sample_mgen = um.RectangleMaskGenerator(128, 128, (96, 128-24, 128, 24))
+    sample_mgen = CenterMaskGenerator(128, 128, 24./64)#um.RectangleMaskGenerator(128, 128, (96, 128-24, 128, 24))
     mask = sample_mgen.gen(1)[0]
 
     feed_dict = vl.make_feed_dict(d)

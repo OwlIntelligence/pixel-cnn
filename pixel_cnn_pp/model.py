4edb04a6ba966c0263de0ead2ba3b3e3ea8630da
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-19 16:30:16 +0800

    sync

--- pixel_cnn_pp/model.py
+++ pixel_cnn_pp/model.py
@@ -45,8 +45,8 @@ def model_spec(x, gh=None, sh=None, ch=None, init=False, ema=None, dropout_p=0.5
 
         if spatial_conditional and sh is not None:
             with arg_scope([nn.conv2d], nonlinearity=resnet_nonlinearity):
-                sh = nn.conv2d(sh, 2*nr_filters, filter_size=[3,3], stride=[1,1], pad='VALID')
-                sh = nn.conv2d(sh, 2*nr_filters, filter_size=[3,3], stride=[1,1], pad='VALID')
+                sh = nn.conv2d(sh, 2*nr_filters, filter_size=[3,3], stride=[1,1], pad='SAME')
+                sh = nn.conv2d(sh, 2*nr_filters, filter_size=[3,3], stride=[1,1], pad='SAME')
                 sh_2 = nn.conv2d(sh, nn.int_shape(sh)[-1], filter_size=[3,3], stride=[2,2], pad='SAME')
                 sh_4 = nn.conv2d(sh_2, nn.int_shape(sh)[-1], filter_size=[3,3], stride=[2,2], pad='SAME')
             if ch is not None:

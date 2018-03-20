9561fc9245d0eedc3cff68096b0a1160bc18f333
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-20 17:23:47 +0800

    sync

--- pixel_cnn_pp/nn.py
+++ pixel_cnn_pp/nn.py
@@ -345,9 +345,9 @@ def latent_deconv_net(h, scale_factor=1, nonlinearity=concat_elu, conv=deconv2d,
     return h
 
 @add_arg_scope
-def deconv_net(z, z_shape=[8,8,10]):
+def deconv_net(z):
     with tf.variable_scope("deconv_net"):
-        net = tf.reshape(z, [-1]+z_shape)
+        #net = tf.reshape(z, [-1]+z_shape)
 
         net = conv2d(net, 512, filter_size=[5,5], stride=[2,2], pad='SAME', nonlinearity=tf.nn.elu)
         net = conv2d(net, 256, filter_size=[5,5], stride=[2,2], pad='SAME', nonlinearity=tf.nn.elu)

985ec6a858fa936c16654257a353d6230a716f89
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-20 11:41:44 +0800

    sync

--- test_pixelcnn.py
+++ test_pixelcnn.py
@@ -478,7 +478,7 @@ with tf.Session(config=config) as sess:
     feed_dict = vl.make_feed_dict(d)
     zs = sess.run(vl.zs, feed_dict=feed_dict)
     sample_x = []
-    mask = sample_mgen.gen(1)[0]
+    mask = um.CenterMaskGenerator(128,128,0.25).gen(1)[0]
     for i in range(args.num_samples):
         completed = complete(sess, data=d, mask=mask, z=np.concatenate(zs, axis=0))
         sample_x.append(completed)

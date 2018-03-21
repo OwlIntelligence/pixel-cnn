d984747a6bcb79b601ae451db750a7fedafd17cf
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-21 18:03:31 +0800

    sync

--- test.py
+++ test.py
@@ -32,10 +32,8 @@ with tf.Session(config=config) as sess:
 
 
     feed_dict = v.make_feed_dict(data, test_mgen)
-    print(v.xs)
-    print(v.mxs)
     ret = sess.run([v.mxs, v.x_hats], feed_dict=feed_dict)
-    mx, x_hat = ret[:len(ret)//2], ret[len(ret)//2:]
+    mx , x_hat = ret[0], ret[1:]
     mx, x_hat = np.concatenate(mx, axis=0), np.concatenate(x_hat, axis=0)
     mx = np.rint(mx*255.)
     x_hat = np.rint(x_hat*255.)

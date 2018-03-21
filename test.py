bf9ee65659286f19c08dce062c94a56ff78b7300
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-21 17:51:15 +0800

    sync

--- test.py
+++ test.py
@@ -21,8 +21,9 @@ with tf.Session(config=config) as sess:
 
     v.load_vae(sess, v.saver)
 
-    test_mgen = m.CenterMaskGenerator(128, 128, .5)
+    test_mgen = m.RandomRectangleMaskGenerator(64, 64)
 
+    data = next(test_data)
     data = next(test_data)
 
     # img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
@@ -31,20 +32,15 @@ with tf.Session(config=config) as sess:
 
 
     feed_dict = v.make_feed_dict(data, test_mgen)
-    sample_x = sess.run(v.x_hats, feed_dict=feed_dict)
-    sample_x = np.concatenate(sample_x, axis=0)
+    ret = sess.run([v.mxs, v.x_hats], feed_dict=feed_dict)
+    mx, x_hat = ret[:len(ret)//2], ret[len(ret)//2:]
+    mx, x_hat = np.concatenate(mx, axis=0), np.concatenate(x_hat, axis=0)
+    mx = np.rint(mx*255.)
+    x_hat = np.rint(x_hat*255.)
 
-    img_tile = plotting.img_tile(sample_x[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
-    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
-    plotting.plt.savefig(os.path.join("plots",'%s_vae_recon.png' % (v.FLAGS.data_set)))
+    from PIL import Image
+    img = Image.fromarray(uf.tile_images(mx.astype(np.uint8), size=(4,4)), 'RGB')
+    img.save(os.path.join("plots", '%s_vae_ori_%s.png' % (args.data_set, "test")))
 
-    data = np.cast[np.float32](data/255.)
-    sample_x = sample_x - data
-
-    img_tile = plotting.img_tile(sample_x[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
-    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
-    plotting.plt.savefig(os.path.join("plots",'%s_vae_diff.png' % (v.FLAGS.data_set)))
-
-    img_tile = plotting.img_tile(data[:25], aspect_ratio=1.0, border_color=1.0, stretch=True)
-    img = plotting.plot_img(img_tile, title=v.FLAGS.data_set + ' samples')
-    plotting.plt.savefig(os.path.join("plots",'%s_vae_ori.png' % (v.FLAGS.data_set)))
+    img = Image.fromarray(uf.tile_images(mx.astype(np.uint8), size=(4,4)), 'RGB')
+    img.save(os.path.join("plots", '%s_vae_recon_%s.png' % (args.data_set, "test")))

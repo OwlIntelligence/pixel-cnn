c28accdf2af6be0a69e3a8b9197a65b4088d9552
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-22 22:07:27 +0800

    sync

--- inspect_vae.py
+++ inspect_vae.py
@@ -33,7 +33,7 @@ with tf.Session(config=config) as sess:
     locs, log_vars = np.concatenate(ret[:len(ret)//2], axis=0), np.concatenate(ret[len(ret)//2:], axis=0)
     scale = np.sqrt(np.exp(log_vars))
 
-    img_id = 1
+    img_id = 2
 
     locs = np.array([locs[img_id] for i in range(32*9)])
     for i in range(32):

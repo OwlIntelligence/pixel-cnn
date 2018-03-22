dcda0b31145d1e39c7dc2ae0a7dc62e087318e54
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-22 12:21:56 +0800

    sync

---
+++ inspect_vae.py
@@ -0,0 +1,32 @@
+import os
+
+import sys
+import json
+import argparse
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from pixel_cnn_pp import nn
+from pixel_cnn_pp.model import model_spec
+from utils import plotting
+import utils.mask as um
+import utils.mfunc as uf
+import utils.grid as grid
+
+
+
+import vae_loading as vl
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as sess:
+
+    vl.load_vae(sess, vl.saver)
+
+    d = next(test_data)
+
+    feed_dict = vl.make_feed_dict(d)
+    ret = sess.run([vl.locs+vl.log_vars, feed_dict=feed_dict)
+    print(ret)

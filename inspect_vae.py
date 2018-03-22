92d50b104b4a58793ec2b36f19a2b59be3dfff1e
Jin Xu <aaron.jin.xu@gmail.com>
2018-03-22 18:05:07 +0800

    sync

--- inspect_vae.py
+++ inspect_vae.py
@@ -35,8 +35,8 @@ with tf.Session(config=config) as sess:
     locs = np.array([locs[7] for i in range(32*3)])
     for i in range(32):
         s = scale[7][i]
-        locs[i*3][i] += 3*s
-        locs[i*3+2][i] -= 3*s
+        locs[i*3][i] += 5*s
+        locs[i*3+2][i] -= 5*s
 
     feed_dict = vl.make_feed_dict_z(locs)
     ret = sess.run(vl.x_hats, feed_dict=feed_dict)
@@ -45,5 +45,5 @@ with tf.Session(config=config) as sess:
     sample_x = np.rint(sample_x * 255.)
 
     from PIL import Image
-    img = Image.fromarray(uf.tile_images(sample_x.astype(np.uint8), size=(32, 3)), 'RGB')
+    img = Image.fromarray(uf.tile_images(sample_x.astype(np.uint8), size=(9,9)), 'RGB')
     img.save(os.path.join("plots", '%s_vae64_%s.png' % (vl.FLAGS.data_set, "test")))
